564fd9a923c7d6bd9cd28902103ba6c6c3adb15e
Thom204 <tmonnier@unal.edu.co>
2026-02-11 17:30:00 -0500

    js final

--- js/gallery.js
+++ js/gallery.js
@@ -5,10 +5,40 @@ function upDate(previewPic){
     var d = document.querySelector("#image");
     d.style.backgroundImage = "url("+previewPic.src+")";
     d.innerHTML = previewPic.alt;
-	}
+}
+
+function blUpdate(previewPic) {
+    previewPic.style.border = "5px solid aquamarine"
+    upDate(previewPic)
+}
+
+function blUnDo(previewPic) {
+    previewPic.style.border = "10px solid black"
+    unDo()
+}
 
 function unDo(){
     var d = document.querySelector("#image");
-		d.style.backgroundImage = "url('')";
+	d.style.backgroundImage = "url('')";
     d.innerHTML = "Hover over an image below to display here.";
-	}
+}
+
+function addTabIndex() {
+    var n = 1
+    for (const img of document.querySelectorAll(".preview")) {
+        img.tabIndex = n
+        n++
+    }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    document.querySelectorAll(".preview").forEach(img => {
+        img.onmouseover = () => upDate(img)
+        img.onfocus = () => blUpdate(img)
+        img.onblur = () => blUnDo(img)
+        img.onmouseout = unDo
+    })
+
+    addTabIndex()
+    console.log("tabfocus added")
+})
